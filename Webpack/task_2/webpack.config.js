0859879f33d68dddd77ac1b37ad156da4ab25fb5
inesouazene <inesouazene@gmail.com>
2025-10-08 00:51:31 +0200

    ...

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
+          'file-loader',
           {
             loader: 'image-webpack-loader',
           },
