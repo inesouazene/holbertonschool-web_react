3bdb6c255f10cbeca50dfd35e165aff4ac6a5fdf
inesouazene <inesouazene@gmail.com>
2025-10-08 00:47:13 +0200

    ...

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -20,22 +20,6 @@ module.exports = {
         use: [
           {
             loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-            },
           },
         ],
       },
