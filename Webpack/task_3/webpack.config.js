9394a2412d6a9ce74d4d395bcc79de52f06bea33
inesouazene <inesouazene@gmail.com>
2025-10-09 08:53:50 +0200

    update task

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, './public'),
     },
     port: 8564,
     open: true,
