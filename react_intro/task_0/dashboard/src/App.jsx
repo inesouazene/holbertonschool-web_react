3e5726b55e78bcd7158b5c154a5a2d850ec7b594
inesouazene <inesouazene@gmail.com>
2026-03-07 16:06:28 +0100

    add files for task 0

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import holbertonLogo from './assets/holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {currentYear} - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
