294bcbecc257286f1a6b5c9fbd534f049b2c902f
inesouazene <inesouazene@gmail.com>
2026-04-03 15:56:55 +0200

    update

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,14 +1,9 @@
 // function getCurrentYear() that returns the current year
-const getCurrentYear = () => {
+export function getCurrentYear() {
   return new Date().getFullYear();
 }
 
 // function getFooterCopy(isIndex) that returns Holberton School when it's true, otherwise it'll return Holberton School main dashboard
-const getFooterCopy= (isIndex) => {
+export function getFooterCopy(isIndex) {
     return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 } 
-
-export {
-    getCurrentYear,
-    getFooterCopy,
-};
