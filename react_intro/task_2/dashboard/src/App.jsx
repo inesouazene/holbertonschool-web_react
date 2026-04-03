7f8fc277fb67f84658f3fc291590c36f51496b62
inesouazene <inesouazene@gmail.com>
2026-04-03 16:29:32 +0200

    add files for task 3

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,31 @@
+import holbertonLogo from './assets/holberton-logo.jpg';
+import './App.css';
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <>
+    <div className='root-notifications'>
+      <Notifications />
+    </div>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email </label>
+        <input type='email' id='email' name='email' />
+        <label htmlFor='password'>Password </label>
+        <input type='password' id='password' name='password' />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getCurrentYear()} {getFooterCopy(false)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
